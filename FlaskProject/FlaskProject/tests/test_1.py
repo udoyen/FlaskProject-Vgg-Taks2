fe8f2c25042b24e08f07afef74a47f9addea0d6d
george <datameshprojects@gmail.com>
2020-03-31 13:58:26 +0100

    Add project files.

---
+++ FlaskProject/FlaskProject/tests/test_1.py
@@ -0,0 +1,27 @@
+import unittest
+import FlaskProject
+
+
+class TestBasic(unittest.TestCase):
+
+    def setUp(self):
+        FlaskWebProject2.app.testing = True
+        self.app = FlaskWebProject2.app.test_client()
+
+    def test_homepage(self):
+        response = self.app.get('/', follow_redirects = True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_rootpage(self):
+        response = self.app.get('/home', follow_redirects = True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_contactpage(self):
+        response = self.app.get('/contact', follow_redirects = True)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
+
+if __name__ == '__main__':
+    unittest.main()
