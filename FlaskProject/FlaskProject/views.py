8476753e734c7ff12f236d0f68c279423a58c5c1
george <datameshprojects@gmail.com>
2020-04-01 08:29:34 +0100

    updated views file

--- FlaskProject/FlaskProject/views.py
+++ FlaskProject/FlaskProject/views.py
@@ -4,7 +4,7 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
-import app
+from FlaskProject import FlaskProject as app
 
 @app.route('/')
 @app.route('/home')
